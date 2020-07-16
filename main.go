1f21545c0a78138937c6cfb9f06aa70d8a66f81d
Eddie Alvarez <eddieaugusto040@gmail.com>
2020-07-15 23:55:22 -0600

    log change

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/EddieAlvarez01/administrator_courses/dao"
-
-	"github.com/EddieAlvarez01/administrator_courses/routes"
-	"github.com/gorilla/mux"
+	"github.com/EddieAlvarez01/administrator_courses/dao/mongodb"
 )
 
 func main() {
-
-	//CONFIG ROUTES
-	r := mux.NewRouter()
-	router := r.PathPrefix("/api").Subrouter()
-	routes.RegisterRoutesPersons(router, dao.PersonImpl{})
-
-	//SERVER
-	fmt.Println("Server on port 7000")
-	log.Fatal(http.ListenAndServe(":7000", router))
-
+	db := mongodb.GetConnection()
+	defer db.Disconnect(context.TODO())
+	fmt.Println("Database is connected")
 }
